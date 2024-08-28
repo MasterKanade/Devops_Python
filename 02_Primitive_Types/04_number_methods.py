b163d5face260e75413930ada21617b6f016d0ca
Mayur Kanade <mastermayurkanade@gmail.com>
2024-08-28 13:45:58 +0530

    In-built mathematical methods for the numbers

---
+++ 02_Primitive_Types/04_number_methods.py
@@ -0,0 +1,15 @@
+# to import some mathematical in built functions and not all
+import math
+# Round function can round the number which is nearest to the whole number
+print(round(2.9))  # the nearest whole number is 3 therefore, it will print 3
+print(round(2.4))  # the nearest whole number is 2 therefore, it will print 2
+# It ranges from 0.0 to 0.5 for the lower whole number and 0.6 to 0.9 for the upper.
+
+# The absolute number will return the absolute value of the number even if it is negative
+print(abs(-2.9))  # it will print the positive 2.9
+
+# for all the mathematical operations
+print(math.ceil(2.2))  # it will print the 3
+
+# for getting factorial number
+print(math.factorial(5))  # it will print the factorial of 5 which is : 120
